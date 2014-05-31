b4e4d8e0102041dbc09899b6d78f191ae97885c1
kamem <develo0@gmail.com>
2014-05-31 12:19:40 +0900

    ちょっとづつ勉強すすめる。

--- js/common.js
+++ js/common.js
@@ -1,27 +1,42 @@
-var __extends = this.__extends || function (d, b) {
-    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
-    function __() { this.constructor = d; }
-    __.prototype = b.prototype;
-    d.prototype = new __();
-};
-var Parent = (function () {
-    function Parent(name) {
-        this.name = name;
-    }
-    Parent.prototype.move = function (meters) {
-        console.log(this.name + " moved " + meters + "m.");
-    };
-    return Parent;
-})();
+var x = 10;
 
-var Child = (function (_super) {
-    __extends(Child, _super);
-    function Child() {
-        _super.apply(this, arguments);
+// any type
+var x3, str = "hello", obj = null;
+
+//関数を定義。 function 関数名(引数名:型...):戻り値
+function add(x, y) {
+    return x + y;
+}
+
+add(1, 1);
+
+function f(point) {
+    console.log(point.x);
+    console.log(point.y);
+}
+
+var o1 = { x: 1, y: 2 };
+var o2 = { x: 1, y: 2 };
+var o3 = { x: 1, y: 2 };
+f(o1);
+f(o2);
+f(o3);
+
+//引数をPoint型でなく、下記のように書くことも可能
+function f2(point) {
+}
+
+var Util;
+(function (Util) {
+    var s = "hello";
+    function f() {
+        return s;
     }
-    Child.prototype.move = function () {
-        console.log("slithering...");
-        _super.prototype.move.call(this, 5);
-    };
-    return Child;
-})(Parent);
+    Util.f = f;
+})(Util || (Util = {}));
+
+// console.log(M.s);　←変数sはexportがついていないので参照できない
+console.log(Util.f());
+
+var u = Util;
+console.log(u.f);
--- ts/common.ts
+++ ts/common.ts
@@ -1,18 +1,54 @@
-class Parent {
-    name: string;
- 
-    constructor(name: string) {
-        this.name = name;
-    }
- 
-    move(meters: number): void {
-        console.log(this.name + " moved " + meters + "m.");
-    }
+var x:number = 10;
+
+// any type
+var x3:string,
+	str:string = "hello",
+	obj = null; 
+	
+	
+//関数を定義。 function 関数名(引数名:型...):戻り値
+function add(x:number, y:number):number {
+    return x + y;
+}
+
+add(1,1);
+
+interface Point {
+ x:number;
+ y:number;
 }
  
-class Child extends Parent {
-    move(): void {
-        console.log("slithering...");
-        super.move(5);
+function f(point:Point) {
+  console.log(point.x);
+  console.log(point.y);
+}
+ 
+var o1 = {x:1,y:2};
+var o2:Point = {x:1,y:2};
+var o3 = <Point>{x:1,y:2};
+f(o1);
+f(o2);
+f(o3);
+
+
+interface Point2 {
+ x:number;
+ y?:number;
+}
+ 
+//引数をPoint型でなく、下記のように書くことも可能
+function f2(point:{x:number;y?:number;}) {
+}
+
+
+module Util {
+    var s = "hello";
+    export function f() {
+        return s;
     }
 }
+// console.log(M.s);　←変数sはexportがついていないので参照できない
+console.log(Util.f());
+
+import u = Util;
+console.log(u.f);
